e9787aab384847b3dd9e9dda9d2daebe163824bb
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-04-23 23:15:22 +0900

    add test

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -10,14 +10,62 @@ require('chai')
 contract('KryotoBird', (accounts) => {
       let contract;
 
+      beforeEach( async() => {
+            contract = await KryptoBird.deployed();
+      });
+
       describe('deployment', async() => {
             it('deploys successfuly', async() => {
-                  contract = await KryptoBird.deployed();
                   const address = contract.address;
                   assert.notEqual(address, '');
                   assert.notEqual(address, null);
                   assert.notEqual(address, undefined);
                   assert.notEqual(address, 0x0);
             });
+
+            it('contract name check', async() => {
+                  const name = await contract.name();
+                  assert.equal(name, 'KryptoBird');
+            });
+
+            it('contract symbol check', async() => {
+                  const symbol = await contract.symbol();
+                  assert.equal(symbol, 'KBIRDZ');
+            });
+      });
+
+      describe('minting', async() => {
+            it('creates a new token', async() => {
+                  const result = await contract.mint('https...1');
+                  const totalSupply = await contract.totalSupply();
+                  // Succees
+                  assert.equal(totalSupply, 1);
+                  const event = result.logs[0].args;
+                  assert.equal(event._from, '0x0000000000000000000000000000000000000000', 'from is contract address');
+                  assert.equal(event._to, accounts[0], 'to is msg.sender');
+                  // Failure
+                  await contract.mint('https...1').should.be.rejected;
+            });
+      });
+
+      describe('indexing', async() => {
+            it('lists KryptoBirdz', async() => {
+                  await contract.mint('https...2');
+                  await contract.mint('https...3');
+                  await contract.mint('https...4');
+                  const totalSupply = await contract.totalSupply();
+
+                  let result = [];
+                  let KryptoBird;
+
+                  for(let i = 1; i < totalSupply; i++) {
+                        KryptoBird = await contract.Kryptobirdz(i - 1);
+                        result.push(KryptoBird);
+                  }
+
+                  let expected = ['https...1', 'https...2', 'https...3', 'https...4'];
+                  assert.equal(result.join(','), expected.join(','));
+            });
       });
 });
+
